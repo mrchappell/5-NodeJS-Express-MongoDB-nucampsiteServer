8d5b723710baaedb633ef427d27bc79de954cd24
mrchappell <michael.r.chappell@gmail.com>
2020-02-26 21:32:41 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1462789163900990',
+        clientSecret: '097af4baab24d08d35b9862e78518860'
+    }
 }
